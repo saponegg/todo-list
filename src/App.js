478f8f47efb3de789cb5fe090f671b487034c4cc
saponegg <saponegg@gmail.com>
2022-05-23 22:27:58 +0200

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div classsName="">
+      <h1>To do list </h1>
     </div>
   );
 }
