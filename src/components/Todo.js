2e3120365553ca6d0f6b680810d982825b2f75db
saponegg <72230964+saponegg@users.noreply.github.com>
2022-05-24 00:11:59 +0200

    f

---
+++ src/components/Todo.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function Todo() {
+const [edit, setEdit]= useState({
+    id:null,
+    value:''
+
+});
+
+
+
+  return todos.map((todo, index) =>(
+      <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+          <div key={todo.id} onClick={() => completeTodo(todo.Id)}>
+              {todo.text}
+          </div>
+
+          <div className="icons">
+              
+          </div>
+
+      </div>
+  ));
+}
+
+export default Todo
